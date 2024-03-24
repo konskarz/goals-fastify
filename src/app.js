574fcadc0630072acf53946b9a9f2cebe9a6e213
Konstantin Karzanov <k.karzanov@gmail.com>
2024-03-24 14:28:07 +0100

    init plugins

--- src/app.js
+++ src/app.js
@@ -1,6 +1,33 @@
+import mapper from '@platformatic/sql-mapper'
+
 /** @param {import('fastify').FastifyInstance} fastify */
 export default async function app (fastify, options) {
   fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
+  })
+  fastify.register(mapper.plugin, {
+    connectionString: process.env.DATABASE_URL
+  })
+  fastify.register(import('@fastify/sensible'), {
+    sharedSchemaId: 'HttpError'
+  })
+  fastify.register(import('@fastify/jwt'), {
+    secret: process.env.JWT_SECRET
+  })
+  fastify.register(import('@fastify/swagger'), {
+    openapi: {
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT'
+          }
+        }
+      }
+    }
+  })
+  fastify.register(import('@fastify/swagger-ui'), {
+    routePrefix: '/docs'
   })
 }
